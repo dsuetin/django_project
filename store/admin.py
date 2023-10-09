012c2758eaebbfdea8f8e30b2c4b76ac774f93d2
daniil <d.suetin@mts.ai>
2023-10-09 16:33:19 +0300

    update site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin category class"""
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Admin product class"""
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'is_active']
+    list_filter = ['in_stock', 'is_active']
+    prepopulated_fields = {'slug': ('title',)}
--- store/models.py
+++ store/models.py
@@ -1,5 +1,42 @@
+""" Script with with site model description"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Category(models.Model):
+    """Class representing a category"""
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        """Class describing matadata """
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Product(models.Model):
+    """Class representing a product"""
+    category = models.ForeignKey(
+        Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='admin')
+    models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        """Class describing product matadata """
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+    def __str__(self):
+        return str(self.title)
     
