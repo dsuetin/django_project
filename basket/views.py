d8d3a473a3b263b14ff5460f2cdaa9ef492600a6
daniil <d.suetin@mts.ai>
2023-10-25 16:26:56 +0300

    return item

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
+from store.models import Product
+
+# from .basket import Basket
 
 
-# Create your views here.
 def basket_summary(request):
+    # basket = Basket(request)
+    # return render(request, 'store/basket/summary.html', {'basket': basket})
+    print("request, basket_summary", request)
     return render(request, 'store/basket/summary.html')
--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.products_all, name='products_all'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
     # path('search/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
