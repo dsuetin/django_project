3ee6a5b6b0d70558eaaecae045c580bdd46feaf7
dsuetin <suetindaniil@gmail.com>
2023-11-07 22:52:45 +0300

    fix price

--- basket/views.py
+++ basket/views.py
@@ -7,7 +7,7 @@ from .basket import Basket
 
 
 def basket_summary(request):
-    basket = Basket(request)
+    # basket = Basket(request)
     # return render(request, 'store/basket/summary.html', {'basket': basket})
     # print("request, basket_summary", request)
     return render(request, 'store/basket/summary.html')
