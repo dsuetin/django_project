32b3efea787f5f1765fce327b6fddf0f493e5c0f
dsuetin <suetindaniil@gmail.com>
2024-04-22 22:14:11 +0300

    refactoring

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.products_all, name='products_all'),
+    path('', views.products_all, name='store_home'),
     path('item/<slug:slug>/', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
     # path('search/<slug:category_slug>/', views.category_list, name='category_list'),
